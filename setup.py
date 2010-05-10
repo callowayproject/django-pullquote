99a515a180a1b8b462d39fe17cc78836a435445c
Justin Quick <justquick@gmail.com>
2010-05-10 10:59:55 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pullquote',
-      version='.1',
+      version='0.1.1',
       description='',
       long_description='',
       author='Jose Soares',
